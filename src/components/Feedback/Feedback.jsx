cc590e66f7413f43c8c576ed1026020ed236503b
Iryna Bilyk <bilykirochka@gmail.com>
2024-01-19 20:38:57 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -15,6 +15,18 @@ export class Feedback extends Component {
   handleClickBad = () => {
     this.setState(prevState => ({ bad: (prevState.bad += 1) }));
   };
+  countTotalFeedback = () => {
+    const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
+    return totalFeedback;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedbackPercentage = Math.round(
+      (this.state.good / totalFeedback) * 100
+    );
+    return positiveFeedbackPercentage;
+  };
 
   render() {
     return (
@@ -36,6 +48,16 @@ export class Feedback extends Component {
           <p>Good: {this.state.good}</p>
           <p>Neutral: {this.state.neutral}</p>
           <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.countTotalFeedback()}</p>
+          {/* {this.countPositiveFeedbackPercentage() >= 0 && (
+            <p>Positive feedback: {this.countPositiveFeedbackPercentage()}%</p>
+          )} */}
+          <p>
+            Positive feedback:{' '}
+            {this.countPositiveFeedbackPercentage() >= 0 &&
+              this.countPositiveFeedbackPercentage()}
+            %
+          </p>
         </div>
       </div>
     );
