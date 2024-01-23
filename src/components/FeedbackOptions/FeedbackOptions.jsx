d791a68f680527c4d6af66c5e24232fcc41755d5
Iryna Bilyk <bilykirochka@gmail.com>
2024-01-23 13:15:10 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,17 +5,23 @@ export const FeedbackOptions = ({
 }) => {
   return (
     <div>
-      <button type="button" style={{ marginRight: 10 }} onClick={onClickGood}>
+      <button
+        type="button"
+        name="good"
+        style={{ marginRight: 10 }}
+        onClick={onClickGood}
+      >
         Good
       </button>
       <button
         type="button"
+        name="neutral"
         style={{ marginRight: 10 }}
         onClick={onClickNeutral}
       >
         Neutral
       </button>
-      <button type="button" onClick={onClickBad}>
+      <button type="button" name="bad" onClick={onClickBad}>
         Bad
       </button>
     </div>
